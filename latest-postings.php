013db4275ea18a28dee267e5b967f3a2a9b61ed4
Heiko August <post@auge8472.de>
2019-02-13 18:58:59 +0100

    Change: reformat of the HTML-template with HTML5-elements and removal of really last domain specific code

--- latest-postings.php
+++ latest-postings.php
@@ -17,31 +17,18 @@
   <link rel="stylesheet" type="text/css" href="latest.css">
  </head>
  <body>
-  <div id="Seite"><!-- sic! -->
-   <div id="Jump"><!-- directly to content -->
-    <p><a href="#Inhalt" title="jump!">directly to content</a> (skip navigation)
-    <hr>
-   </div><!-- end 'Jump' div -->
-   <div id="navcontainer"><!-- horizontal navigation -->
-   </div><!-- end of navigation -->
-   <p class="p.clr">
-   <div id="Inhalt"><!-- content div -->
-    <!-- <a href="http://blog.slepp.ca/mouldspellbound.php?mod=3">shortlived</a> -->
-    <h1>Latest twenty posts</h1>
-    <ul class="posts">
-     <?php include('LatestPosts.inc.php'); ?>
-</ul>
-    <p class="small">Data are current with <?php echo date('l, j F o H:i T (\E\u\r\o\p\e\/\V\i\e\n\n\a; \U\T\C P)'); ?>.<br>
-    <?php include('OnlineCounter.inc.php'); ?><!-- visitors (registered, guests, bots) -->
-   </div><!-- end of content div -->
-   <p id="foot">The <strong>BIOEQUIVALENCE / BIOAVAILABILITY FORUM</strong> is hosted by<br>
-    <object data="https://static.bebac.at/img/bebac.svg" type="image/svg+xml" width="75" height="50">
-     <param name="src" value="https://static.bebac.at/img/bebac.svg">
-     <img src="https://static.bebac.at/img/bebac-small.gif" width="75" alt="BEBAC" title="">
-    </object>
-    <strong> <span lang="de">Ing. Helmut Schütz</span></strong><br>
-    Established 2004-07-30 | last update 2018-10-20 | document <?php echo date ('Y-m-d', getlastmod()); ?> | GeoLite data created by <a href="https://dev.maxmind.com/geoip/legacy/geolite/" title="GeoLite Country">MaxMind</a>.  <a href="rss.php"><img src="https://static.bebac.at/img/rss.png" width="14" height="14" alt="RSS Feed" title="recent additions in RSS 2.0"></a>
-   </p>
-  </div><!-- end of page div -->
+  <p><a href="#Inhalt" title="jump!">directly to content</a> (skip navigation)</p>
+  <header>
+   <h1>Latest twenty posts</h1>
+  </header>
+  <nav><!-- put in your navigation -->
+  </nav><!-- end of navigation -->
+  <main role="main">
+   <ul class="posts">
+    <?php include('LatestPosts.inc.php'); ?>
+   </ul>
+  </main>
+  <footer><!-- put in your page footer -->
+  </footer><!-- end of page footer -->
  </body>
 </html>
